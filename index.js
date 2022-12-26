ad7a0d4c19a7a190cde494a27386440d08249b43
Amen <jamen.jamne30@gmail.com>
2022-12-26 20:12:29 +0200

    step 3

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
     res.json({message: "ok"})
 })
 
+app.get('/test', (req, res) => {
+    res.json({status:200, message:"ok"})
+})
+
+app.get('/time', (req, res) => {
+    let datetime = new Date()
+    res.json({status:200,message: `${datetime.getHours()}:${datetime.getMinutes()}` })
+})
 
 
 app.listen(3000)
