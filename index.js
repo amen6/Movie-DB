aba3a92e49cc2149b87980ee0d1708820f28e0c1
Amen <jamen.jamne30@gmail.com>
2022-12-27 16:56:53 +0200

    step 4

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ app.get('/test', (req, res) => {
 
 app.get('/time', (req, res) => {
     let datetime = new Date()
-    res.json({status:200,message: `${datetime.getHours()}:${datetime.getMinutes()}` })
+    res.json({status:200, message: `${datetime.getHours()}:${datetime.getMinutes()}`})
+})
+
+app.get('/hello/:username', (req, res) => {
+    res.json({status:200, message: `Hello, ${req.params.username}`})
+})
+
+app.get('/search', (req, res) => {
+    console.log(req.query.s)
+    if(req.query.s == "" || req.query.s == undefined) {
+        console.log(req.query.s)
+        res.json({status:500, error:true, message:"you have to provide a search" })
+    } 
+    else {
+        res.json({status:200, message:"ok", data: req.query.s})
+    }
 })
 
 
