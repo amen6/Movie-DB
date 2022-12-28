5dd1fc2d733c2b4ad86d1c4e85fec64186d0804d
Amen <jamen.jamne30@gmail.com>
2022-12-28 14:39:41 +0200

    step 8

--- routers/movies.js
+++ routers/movies.js
@@ -8,8 +8,35 @@ const movies = [
     { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ]
 
-router.get("/create", (req, res) => {    
-    res.json({message: "create"})
+router.get("/create", (req, res) => {  
+    let movie = req.query;
+    const checkMovie = obj => obj.title === movie.title;
+    if(!movie.title || movie.year.length != 4 || isNaN(movie.year)) {
+        res.status(403)
+        .json({status:403, error:true, message:'you cannot create a movie without providing a title and a year'})
+    }
+    else if(movies.some(checkMovie)) {
+        res.status(400)
+        .json({status:400, error:true, message:'movie name is already used'})
+    } 
+    else if(parseFloat(movie.rating) > 10) {
+        let newMovie = {
+            title: movie.title,
+            year: parseInt(movie.year),
+            rating: 10
+        };
+        movies.push(newMovie);
+        res.status(200).json({message: movies})
+    }
+    else {
+        let newMovie = {
+            title: movie.title,
+            year: parseInt(movie.year),
+            rating: parseFloat(movie.rating) || 4
+        };
+        movies.push(newMovie);
+        res.status(200).json({message: movies})
+    }
 })
 
 router.get("/read", (req, res) => {
